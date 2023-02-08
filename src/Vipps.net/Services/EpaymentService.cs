722b3637ab5c674203b21cecaa5f2ae983c9fe64
Sven Erik Pytterud <sven.erik.pytterud@vipps.no>
2023-02-08 09:29:15 +0100

    merge fix

--- src/Vipps.net/Services/EpaymentService.cs
+++ src/Vipps.net/Services/EpaymentService.cs
@@ -1,4 +1,17 @@
-﻿namespace Vipps.Services
+﻿using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+using Vipps.Helpers;
+using Vipps.Models;
+using Vipps.Models.Epayment.CancelPayment;
+using Vipps.Models.Epayment.CapturePayment;
+using Vipps.Models.Epayment.CreatePayment;
+using Vipps.Models.Epayment.CreatePaymentRequest;
+using Vipps.Models.Epayment.ForceApprove;
+using Vipps.Models.Epayment.GetPaymentEventLog;
+using Vipps.Models.Epayment.GetPaymentResponse;
+using Vipps.Models.Epayment.RefundPayment;
+
+namespace Vipps.Services
 {
     public class EpaymentService
     {
