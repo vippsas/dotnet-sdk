e122ebef2d7ea436e9f55800c45dabadf0b54077
Roar Mjelde <roar.mjelde@vipps.no>
2023-02-09 12:15:42 +0100

    Added test for array serialization

--- src/Tests/Vipps.net.Tests/VippsRequestSerializerTests.cs
+++ src/Tests/Vipps.net.Tests/VippsRequestSerializerTests.cs
@@ -42,6 +42,40 @@ namespace Vipps.net.Tests
         }
 
         [TestMethod]
+        public void Can_Serialize_With_Extra_Parameters_Array()
+        {
+            InitiateSessionRequest initiateSessionRequest =
+                new()
+                {
+                    Transaction = new PaymentTransaction()
+                    {
+                        Amount = new Amount() { Currency = "NOK", Value = 49000 },
+                        PaymentDescription = "Hei"
+                    },
+                    Configuration = new CheckoutConfig() { Elements = Elements.PaymentOnly },
+                    ExtraParameters = new
+                    {
+                        Configuration = new { AcceptedPaymentMethods = new[] { "WALLET", "CARD" } }
+                    }
+                };
+            var serializedRequest = VippsRequestSerializer.SerializeVippsRequest(
+                initiateSessionRequest
+            );
+            Assert.IsNotNull(serializedRequest);
+            Assert.AreNotEqual("", serializedRequest);
+            var deserialized = JsonSerializer.Deserialize<JsonElement>(serializedRequest);
+            deserialized.TryGetProperty("ExtraParameters", out var deserializedExtraParameters);
+            Assert.AreEqual(JsonValueKind.Undefined, deserializedExtraParameters.ValueKind);
+            Assert.AreEqual(
+                JsonValueKind.Array,
+                deserialized
+                    .GetProperty("Configuration")
+                    .GetProperty("AcceptedPaymentMethods")
+                    .ValueKind
+            );
+        }
+
+        [TestMethod]
         public void Can_Serialize_Without_Extra_Parameters()
         {
             InitiateSessionRequest initiateSessionRequest =
