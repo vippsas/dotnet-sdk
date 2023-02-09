0e4bd9549de49f09009b0f3df6d1f16141c5bfb9
Sven Erik Pytterud <sven.erik.pytterud@vipps.no>
2023-02-09 12:29:45 +0100

    Setup config

---
+++ src/Vipps.net/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Vipps.Models;
+
+namespace Vipps.net
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureVipps(IConfiguration configuration, string sectionName)
+        {
+            var settings = new VippsConfiguration();
+            configuration.GetSection(sectionName).Bind(settings);
+        }
+    }
+}
