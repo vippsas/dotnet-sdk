c4c8ba876f44da5f5d21ccb06fbe600e0df9cb66
SvenErikPy <110486952+SvenErikPy@users.noreply.github.com>
2023-02-10 12:46:48 +0100

    Vico 873 use singleton http clients (#10)
    * Use httpclient + static configuration
    
    * Format with csharpier
    
    * fix merge issues
    
    * Use centralized serialization of vippsrequest
    
    * Optional cancellationToken
    
    * Fix test namespace
    
    * remove spaces
    
    * internal baseurl and vippsclient getter
    
    * Remove VoidType
    
    * remove contenttype header
    
    * move adding idempotencykey header to epaymentservice
    
    * remove unncessary type specification
    
    * using abstract base
    
    * inject vippshttpclient

---
+++ src/Vipps.net/Infrastructure/CheckoutServiceClient.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Vipps.net.Helpers;
+
+namespace Vipps.net.Infrastructure
+{
+    internal sealed class CheckoutServiceClient : BaseServiceClient
+    {
+        internal CheckoutServiceClient(IVippsHttpClient vippsHttpClient)
+            : base(vippsHttpClient)
+        {
+            Logger = LoggerFactory
+                .Create((ILoggingBuilder lb) => { })
+                .CreateLogger<CheckoutServiceClient>();
+        }
+
+        protected override async Task<Dictionary<string, string>?> GetHeaders(
+            CancellationToken cancellationToken
+        )
+        {
+            return await Task.FromResult(
+                new Dictionary<string, string>
+                {
+                    { Constants.HeaderNameClientId, VippsConfiguration.ClientId },
+                    { Constants.HeaderNameClientSecret, VippsConfiguration.ClientSecret }
+                }
+            );
+        }
+    }
+}
