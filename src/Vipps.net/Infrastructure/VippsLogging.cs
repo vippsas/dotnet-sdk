660e6beb1d2aab3c1db11e11f94907f63db4359b
Sven Erik Pytterud <sven.erik.pytterud@vipps.no>
2023-02-14 15:12:23 +0100

    remove getter and setter

--- src/Vipps.net/Infrastructure/VippsLogging.cs
+++ src/Vipps.net/Infrastructure/VippsLogging.cs
@@ -4,11 +4,6 @@ namespace Vipps.net.Infrastructure
 {
     internal static class VippsLogging
     {
-        internal static ILoggerFactory _loggerFactory;
-        internal static ILoggerFactory LoggerFactory
-        {
-            get { return _loggerFactory; }
-            set { _loggerFactory = value; }
-        }
+        internal static ILoggerFactory LoggerFactory { get; set; }
     }
 }
