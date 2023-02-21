967c9d1beca68e469300aacf8031f25bfef1e035
Sven Erik Pytterud <sven.erik.pytterud@vipps.no>
2023-02-21 16:05:14 +0100

    WIP

---
+++ src/Vipps.net.Codegen/CodegenSettings.cs
@@ -0,0 +1,27 @@
+﻿using NSwag.CodeGeneration.CSharp;
+
+namespace Vipps.net.Codegen
+{
+    internal sealed class CodegenSettings
+    {
+        internal string OpenApiJsonPath { get; init; }
+        internal string RelativeFilePath { get; init; }
+        internal CSharpClientGeneratorSettings ClientGeneratorSettings { get; init; }
+
+        internal CodegenSettings(
+            string openApiJsonPath,
+            string className,
+            string baseNamespace,
+            string relativeFilePath
+        )
+        {
+            OpenApiJsonPath = openApiJsonPath;
+            RelativeFilePath = relativeFilePath;
+            ClientGeneratorSettings = new CSharpClientGeneratorSettings
+            {
+                ClassName = className,
+                CSharpGeneratorSettings = { Namespace = baseNamespace }
+            };
+        }
+    }
+}
---
+++ src/Vipps.net.Codegen/Program.cs
@@ -0,0 +1,29 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+using Vipps.net.Codegen;
+
+internal sealed class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var httpClient = new HttpClient();
+
+        var options = new CodegenSettings(
+            "http://localhost:5000/swagger/merchant-v3/swagger.json",
+            "VippsCheckout",
+            "Vipps.net.Models",
+            "../Vipps.net/Models/CheckoutModels.cs"
+        );
+        Console.WriteLine($"Fetching from {options.OpenApiJsonPath}");
+        var retrievedJson = await httpClient.GetStringAsync(options.OpenApiJsonPath);
+        Console.WriteLine($"Retrieved from {options.OpenApiJsonPath}");
+        var document = await OpenApiDocument.FromJsonAsync(retrievedJson);
+        Console.WriteLine($"Generated document from {options.OpenApiJsonPath}");
+
+        var generator = new CSharpClientGenerator(document, options.ClientGeneratorSettings);
+        var code = generator.GenerateFile();
+        Console.WriteLine($"Generated code from {options.OpenApiJsonPath}");
+        File.WriteAllText(options.RelativeFilePath, code);
+        Console.WriteLine($"Wrote file {options.RelativeFilePath}");
+    }
+}
